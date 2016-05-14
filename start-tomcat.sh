1ab7396a2bca388613f2ec79fe77813d2e3af6ff
btooncall <btooncall@bullhorn.com>
2016-05-14 16:56:27 +0100

    Update start-tomcat.sh

--- start-tomcat.sh
+++ start-tomcat.sh
@@ -2,5 +2,6 @@
 
 export FR_OPTS="-javaagent:/opt/fusionreactor/fusionreactor.jar=address=8088 -Dfradminpassword=$FRPASSWD -Dfrlicense=$FRLICENSE"
 export JAVA_OPTS="$JAVA_OPTS $FR_OPTS"
+export CLASSPATH="${CLASSPATH}:/opt/tomcat/bin/commons-daemon.jar
 
 exec /opt/tomcat/bin/catalina.sh run
