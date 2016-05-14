fe21268e63747b5bbe0f2824c2eebe499cf26e9b
btooncall <btooncall@bullhorn.com>
2016-05-14 17:25:03 +0100

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -5,7 +5,7 @@ export FR_OPTS="-javaagent:/opt/fusionreactor/fusionreactor.jar=address=8088 -Df
 
 export JAVA_OPTS="${JAVA_OPTS} -server -Xmx${XMX} -Xms${XMS} -XX:MaxPermSize=${MaxPermSize}"
 
-export JAVA_OPTS="${JAVA_OPTS} ${FR_OPTS}"
+export JAVA_OPTS="${JAVA_OPTS} ${FR_OPTS} ${JavaBullhornOptions}"
 
 # Add basic stuff to classpath (tomcat-juli, commons-daemon, and bootstrap)
 export CLASSPATH="${CLASSPATH}:/opt/tomcat/bin/commons-daemon.jar:/opt/tomcat/bin/bootstrap.jar:/opt/tomcat/bin/tomcat-juli.jar
