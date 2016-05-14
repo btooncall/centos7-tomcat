c24700a549c0cbd88b77c6d41e4d97f07718a967
btooncall <btooncall@bullhorn.com>
2016-05-14 17:01:31 +0100

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# set up fusion reactor options 
+export FR_OPTS="-javaagent:/opt/fusionreactor/fusionreactor.jar=address=8088 -Dfradminpassword=$FRPASSWD -Dfrlicense=$FRLICENSE"
+
+
+export JAVA_OPTS="$JAVA_OPTS $FR_OPTS"
+
+# Add basic stuff to classpath (tomcat-juli, commons-daemon, and bootstrap)
+export CLASSPATH="${CLASSPATH}:/opt/tomcat/bin/commons-daemon.jar:/opt/tomcat/bin/bootstrap.jar:/opt/tomcat/bin/tomcat-juli.jar
